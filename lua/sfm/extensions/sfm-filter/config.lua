98223d6d3ce9df235107da7c1617086383cddf5a
M. Nabil Adani <nblid48@gmail.com>
2024-10-13 11:37:36 +0700

    refactor: added an option to hide files from entries based on file extension (#2)

--- lua/sfm/extensions/sfm-filter/config.lua
+++ lua/sfm/extensions/sfm-filter/config.lua
@@ -5,6 +5,7 @@ local M = {
 local default_config = {
   show_hidden = false,
   ignore_names = {},
+  ignore_extensions = {},
   mappings = {
     toggle_filter = { "." },
   },
@@ -24,6 +25,12 @@ function M.setup(opts)
   if opts.ignore_names ~= nil and type(opts.ignore_names) == "table" then
     for _, ignore_name in ipairs(opts.ignore_names) do
       M.opts.ignore_names[ignore_name] = true
+    end
+  end
+
+  if opts.ignore_extensions ~= nil and type(opts.ignore_extensions) == "table" then
+    for _, ignore_extension in ipairs(opts.ignore_extensions) do
+      M.opts.ignore_extensions[ignore_extension] = true
     end
   end
 
